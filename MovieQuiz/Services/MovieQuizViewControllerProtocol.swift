73f4bd3e50f5fd356e175848104ad5630339c4ae
Emiliano <emil.azmedov@mail.ru>
2025-01-17 16:09:26 +0300

    Создан MovieQuizViewControllerProtocol, Контроллер подписан под протокол

---
+++ MovieQuiz/Services/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,10 @@
+protocol MovieQuizViewControllerProtocol: AnyObject {
+    func show(quiz step: QuizStepViewModel)
+    func show(quiz result: QuizResultsViewModel)
+    
+    func highLightImageBorder(isCorrectAnswer: Bool)
+    
+    func loadingIndicatorIsHidden(_ isHidden: Bool)
+    
+    func showNetworkError(message: String)
+}
